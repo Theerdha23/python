1c695f63541b49a0f06b39f7987b4bf144b1f884
Theerdha23 <theerdhay@gmail.com>
2026-06-30 12:17:44 +0530

    add file

---
+++ data_types/data_types.py
@@ -0,0 +1,16 @@
+phone_payno = 500  # <class 'int'>
+recievers_name = "theerdha"  # <class 'str'>
+is_recieved = True  # <class 'bool'>
+is_not_recieved = False  # <class 'bool'>
+
+# Print types
+print(type(phone_payno))       # <class 'int'>
+print(type(recievers_name))    # <class 'str'>
+print(type(is_recieved))       # <class 'bool'>
+print(type(is_not_recieved))   # <class 'bool'>
+
+# Print memory IDs
+print(id(phone_payno))
+print(id(recievers_name))
+print(id(is_recieved))
+print(id(is_not_recieved))
---
+++ data_types/immutale_and_mm.py
@@ -0,0 +1,40 @@
+m=120
+print(id(m))
+h="harry"
+print(id(h))
+l=["theerdha",2,99]
+print(id(l))
+s={1,2,3}
+print(id(s))
+theerdha = {'name': 'theerdha','age' : 12}
+print(id(theerdha))
+t=(1,2,3)
+print(id(t))
+# print(t)
+# print(h)
+# print(l)
+# print(s)
+# print(theerdha)
+# crtl + / windows
+m=1
+print(m)
+print(id(m))
+l[1] = "harry"
+print(l)
+print(id(l))
+s.add(5)
+print(s)
+print(id(s))
+theerdha['name'] = 'theerdha'
+print(theerdha)
+print(id(theerdha))
+t.__lt__(1)
+print(t)
+print(id(t))
+r = b'4'
+y = bytearray(r)
+print(y)
+print(type(r))
+y[0] = 57
+print(y)
+print(id(r))
