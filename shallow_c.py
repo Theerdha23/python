43e73e833f7f8ed9e00edc54ea7290c1c45530e7
Theerdha23 <theerdhay@gmail.com>
2026-07-02 17:14:15 +0530

    r

--- shallow_c.py
+++ shallow_c.py
@@ -6,3 +6,10 @@ print(shallow_c)
 print(org_list)
 print(id(org_list[0][0]) == id(shallow_c[0][0]))#innner list are same address
 print(id(org_list) == id(shallow_c))#outer listare different
+
+depp_c = copy.deepcopy(org_list)
+depp_c[0][0]='n'
+print(depp_c)
+print(org_list)
+print(id(org_list[0][0]) == id(depp_c[0][0]))
+print(id(org_list) == id(depp_c))
