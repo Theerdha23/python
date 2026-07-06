065ab52e0c59d7f74edd8f9057c313e888961aa9
Theerdha23 <theerdhay@gmail.com>
2026-07-06 15:45:57 +0530

    r

--- functions/AR.py
+++ functions/AR.py
@@ -19,4 +19,13 @@ b = int(input("Enter the second number: "))
 
 print(div(mul(add(a,b),sub(a,b)),10))
 
+#postional arg and locational arg
+
+def trip_details(total_fare,driver_name,pickup,drop):
+    print(f" total fare is {total_fare} and  driver name is {driver_name} and  pickup is {pickup} and  drop is {drop}")
+
+trip_details(1000,"theerrdha",'loc','loc') #poistional
+trip_details("theerdha",1000,'mun','hyd')
+trip_details(drop = 'you', total_fare= 1000,driver_name= 'mun',pickup= 'hyd') #location arg
+
 
